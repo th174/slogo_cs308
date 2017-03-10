33d208aa1b8e9582306d9a1325103a3b8693ad41
Stone Mathers <smm117@duke.edu>
2017-03-10 00:11:20 -0500

    Can write Funcs/Vars to file with LibraryWriter

---
+++ src/SLogo/FileHandling/ConfigurationWriter.java
@@ -0,0 +1,23 @@
+package SLogo.FileHandling;
+
+import java.io.File;
+
+/**
+ * @author Stone Mathers
+ *
+ */
+public class ConfigurationWriter implements FileWriter {
+
+
+	public ConfigurationWriter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	public void write(File file) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SLogo/FileHandling/LibraryWriter.java
@@ -0,0 +1,66 @@
+package SLogo.FileHandling;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import SLogo.FunctionEvaluate.Environment;
+import SLogo.FunctionEvaluate.Functions.Invokable;
+import SLogo.FunctionEvaluate.Functions.UserFunction;
+import SLogo.FunctionEvaluate.Variables.Variable;
+
+public class LibraryWriter implements FileWriter{
+
+	private Environment myEnvironment;
+	private Map<String, Variable> varMap;
+	private Map<String, Invokable> funcMap;
+	private static final String COMMAND_BUNDLE = "resources/files/writerCommands";
+	public ResourceBundle myCommands = ResourceBundle.getBundle(COMMAND_BUNDLE);
+	private static final String ERROR_BUNDLE = "resources/View/Exceptions";
+	public ResourceBundle myErrors = ResourceBundle.getBundle(ERROR_BUNDLE);
+	
+	public LibraryWriter(Environment env) {
+		myEnvironment = env;
+		varMap = myEnvironment.getAllVars();
+		funcMap = myEnvironment.getLocalFunctions();
+	}
+
+	/**
+	 * Takes in a file and writes the current user functions and variables to it.
+	 * 
+	 * @param file File to which library is written.
+	 */
+	@Override
+	public void write(File file){
+		Path filePath = Paths.get(file.toURI());
+		ArrayList<String> commands = new ArrayList<String>();
+		commands.add(myCommands.getString("defaultUse"));
+		writeVariables(commands);	
+		writeFunctions(commands);
+		try {
+			Files.write(filePath, commands);
+		} catch (IOException e) {
+			throw new RuntimeException(String.format(myErrors.getString("FileWritingError"), file.getName()), e);
+		}
+	}
+	
+	private void writeVariables(List<String> commands){
+		for(String varName: varMap.keySet()){
+			commands.add((String.format(myCommands.getString("Make"), varName, varMap.get(varName))));
+		}
+	}
+	
+	private void writeFunctions(List<String> commands){
+		for(String funcName: funcMap.keySet()){
+			if(funcMap.get(funcName) instanceof UserFunction){
+				commands.add((String.format(myCommands.getString("To"), funcMap.get(funcName).toString())));
+			}
+		}
+	}
+}
