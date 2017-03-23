b0fa8efe2752bf24bdc9c9ea8365190fad5491f9
Stone Mathers <smm117@duke.edu>
2017-03-23 08:50:56 -0400

    Masterpiece

--- src/SLogo/FileHandling/FileWriter.java
+++ src/SLogo/FileHandling/FileWriter.java
@@ -1,3 +1,6 @@
+//This entire file is part of my masterpiece.
+//Stone Mathers
+
 package SLogo.FileHandling;
 
 import java.io.File;
--- src/SLogo/FileHandling/LibraryWriter.java
+++ src/SLogo/FileHandling/LibraryWriter.java
@@ -1,3 +1,6 @@
+//This entire file is part of my masterpiece.
+//Stone Mathers
+
 package SLogo.FileHandling;
 
 import SLogo.FunctionEvaluate.Environment;
